e4de89b210f9267e2d0dbfabc6c958fc73187d34
Vladislav Priske <55079623+wemitwqw@users.noreply.github.com>
2025-05-21 12:38:17 +0300

    plural endpoints;

--- backend/src/main/java/ee/vladislav/backend/controller/ConsumptionController.java
+++ backend/src/main/java/ee/vladislav/backend/controller/ConsumptionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/consumption")
+@RequestMapping("/api/consumptions")
 public class ConsumptionController {
 	private final ConsumptionService consumptionService;
 
--- backend/src/main/java/ee/vladislav/backend/controller/MeteringPointController.java
+++ backend/src/main/java/ee/vladislav/backend/controller/MeteringPointController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/metering-point")
+@RequestMapping("/api/metering-points")
 public class MeteringPointController {
 	private final MeteringPointService meteringPointService;
 
