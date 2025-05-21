36451623c530a2e4bd9d1b78a618aae1d68cc033
Vladislav Priske <55079623+wemitwqw@users.noreply.github.com>
2025-05-21 15:48:14 +0300

    configuration;

--- backend/src/main/java/ee/vladislav/backend/config/AppConfig.java
+++ backend/src/main/java/ee/vladislav/backend/config/AppConfig.java
@@ -1,12 +1,15 @@
 package ee.vladislav.backend.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -20,14 +23,19 @@ public class AppConfig {
 	}
 
 	@Bean
-	public ObjectMapper objectMapper() {
-		return Jackson2ObjectMapperBuilder.json()
-				.modules(new JavaTimeModule())
-				.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-				.build();
-	}
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
 
-	@Bean public RestTemplate restTemplate() {
-		return new RestTemplate();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		objectMapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
+
+		MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+		messageConverter.setObjectMapper(objectMapper);
+
+		restTemplate.setMessageConverters(Collections.singletonList(messageConverter));
+
+		return restTemplate;
 	}
 }
--- backend/src/main/java/ee/vladislav/backend/config/RedisConfig.java
+++ backend/src/main/java/ee/vladislav/backend/config/RedisConfig.java
@@ -5,25 +5,24 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
 	@Bean
-	public RedisCacheConfiguration cacheConfiguration(ObjectMapper objectMapper) {
+	public RedisCacheConfiguration cacheConfiguration() {
 		return RedisCacheConfiguration.defaultCacheConfig()
 				.entryTtl(Duration.ofHours(24))
 				.disableCachingNullValues()
 				.serializeValuesWith(
 						RedisSerializationContext.SerializationPair.fromSerializer(
-								new GenericJackson2JsonRedisSerializer(objectMapper))
+								new JdkSerializationRedisSerializer(getClass().getClassLoader())
+						)
 				);
 	}
 
