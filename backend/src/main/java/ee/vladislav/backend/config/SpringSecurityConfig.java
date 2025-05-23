e1edc502a54b8ca31b35eb7e06cedd5248c0e613
Vladislav Priske <55079623+wemitwqw@users.noreply.github.com>
2025-05-23 11:07:49 +0300

    apps ports changes;

--- backend/src/main/java/ee/vladislav/backend/config/SpringSecurityConfig.java
+++ backend/src/main/java/ee/vladislav/backend/config/SpringSecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -60,7 +61,7 @@ public class SpringSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+		configuration.setAllowedOrigins(List.of("http://localhost:3001"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 		configuration.setAllowCredentials(true);
